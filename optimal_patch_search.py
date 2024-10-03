1f8593b36503613c964cb8f9af64577da476b027
Prisha-Chandiramani <154550366+Prisha-Chandiramani@users.noreply.github.com>
2024-10-03 13:56:10 +0200

    tests à revoir

--- optimal_patch_search.py
+++ optimal_patch_search.py
@@ -31,26 +31,35 @@ def calcul_dist(p,q):
     d = np.sum((p-q)**2)
     return d
 
-def choose_q(omega, p):
+def choose_q(target_region_mask, p, im):
     D={}
-    psi = gray_image_matrix - omega #déterminer comment déterminer psi à partir de omega
-    print(psi)
-    psi_image = Image.fromarray(psi)
-    psi_image.show()
-    print("psi.shape:",psi.shape)
-    for i in range(psi.shape[0]-9):
-        for j in range(psi.shape[1]-9):
-            q = psi[i:i+9,j:j+9]
-            d = calcul_dist(p,q)
-            D[(i,j)]=d
+    #psi = gray_image_matrix - omega #déterminer comment déterminer psi à partir de omega
+    #print(psi)
+    #psi_image = Image.fromarray(psi)
+    #psi_image.show()
+    #print("psi.shape:",psi.shape)
+    source_region_mask = np.logical_not(target_region_mask)
+    for i in range(source_region_mask.shape[0]-9):
+        for j in range(source_region_mask.shape[1]-9):
+            q_mask = source_region_mask[i:i+9,j:j+9]
+            if q_mask == np.array([[True for i in range(9)] for j in range(9)]):
+                q = im[i:i+9,j:j+9]
+                d = calcul_dist(p,q)
+                D[(i,j)]=d
             #print("q.shape:",q.shape)
             #print("q:",q)
             #print("d:",d)
-    return D  
+    minimum_D = min(D, key=D.get) # renvoie la clé de la valeur minimale
+    q_opt = im[minimum_D[0]:minimum_D[0]+9,minimum_D[1]:minimum_D[1]+9]
+
+    return q_opt 
 
 
-omega = np.zeros((61,61))
-omega[12:48,12:48] = gray_image_matrix[12:48,12:48] # patch 10
+#omega = np.zeros((61,61))
+#omega[12:48,12:48] = gray_image_matrix[12:48,12:48] # patch 10
+
+target_region_mask = np.array([[False for i in range(61)] for j in range(61)])
+target_region_mask[12:48,12:48] = True 
 p1 = gray_image_matrix[12:21,12:21] # patch 9
 p1_image = Image.fromarray(p1)
 p1_image.show()
@@ -58,26 +67,27 @@ p2 = gray_image_matrix[30:39,30:39] # patch 9
 p2_image = Image.fromarray(p2)
 p2_image.show()
 
-new_matrix = gray_image_matrix - omega
+new_matrix = gray_image_matrix.copy()
 
-D = choose_q(omega, p1)
-m = min(D, key=D.get) # renvoie la clé de la valeur minimale
-mat = gray_image_matrix-omega
-m_matrix = mat[m[0]:m[0]+9,m[1]:m[1]+9]
-print(m)
-print(m_matrix)
-print(m_matrix.shape)
+D = choose_q(target_region_mask, p1, gray_image_matrix)
+#m = min(D, key=D.get) # renvoie la clé de la valeur minimale
+#mat = gray_image_matrix-omega
+#m_matrix = mat[m[0]:m[0]+9,m[1]:m[1]+9]
+#print(m)
+
+#print(m_matrix)
+#print(m_matrix.shape)
 print(p1)
-new_matrix[12:21,12:21] = m_matrix
+new_matrix[12:21,12:21] = D
 
-D2 = choose_q(omega, p2)
+D2 = choose_q(target_region_mask, p2, gray_image_matrix)
 m2 = min(D2, key=D2.get) # renvoie la clé de la valeur minimale
-mat = gray_image_matrix-omega
-m2_matrix = mat[m2[0]:m2[0]+9,m2[1]:m2[1]+9]
-print(m2)
-print(m2_matrix)
-print(m2_matrix.shape)
+#mat = gray_image_matrix-omega
+#m2_matrix = mat[m2[0]:m2[0]+9,m2[1]:m2[1]+9]
+#print(m2)
+#print(m2_matrix)
+#print(m2_matrix.shape)
 print(p2)
-new_matrix[30:39,30:39] = m2_matrix
+new_matrix[30:39,30:39] = D2
 new_matrix_image = Image.fromarray(new_matrix)
 new_matrix_image.show()
