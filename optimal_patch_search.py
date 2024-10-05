956542209da970b94760ce02a102fc027395cc80
Prisha-Chandiramani <154550366+Prisha-Chandiramani@users.noreply.github.com>
2024-10-05 15:14:21 +0200

    choose_q a l'air de fonctionner

--- optimal_patch_search.py
+++ optimal_patch_search.py
@@ -15,28 +15,35 @@ gray_image = image.convert("L")
 
 gray_image_matrix = np.array(gray_image)
 
-print(gray_image_matrix.shape)
+print("gray image matrix shape: ",gray_image_matrix.shape)
 
-print(gray_image_matrix)
+#print(gray_image_matrix)
 
-gray_image.show()
+#gray_image.show()
 
-p = gray_image_matrix[30:39,30:39]
-print(p.shape)
-print(p)
-p_image = Image.fromarray(p)
-p_image.show()
+#p = gray_image_matrix[30:39,30:39]
+#print(p.shape)
+#print(p)
+#p_image = Image.fromarray(p)
+#p_image.show()
 
-def calcul_dist(p,q):
+## Fonctions
+
+def calcul_dist(p,q, p_mask):
    # dans p, il peut y avoir des valeurs à None
+   #p_mask[i,j] = True si il faut prendre la valeur du pixel dans p qu'elle n'est pas vide
     sum = 0
+    if p.shape != q.shape:
+        raise ValueError("Les deux patchs n'ont pas la même taille")
     for i in range(p.shape[0]):
         for j in range(p.shape[1]):
-            if p[i,j] != None:
-             sum += (p[i,j]-q[i,j])**2   
+            if p_mask[i,j] == True:
+                 # Cast to float64 to prevent overflow
+                diff = float(p[i, j]) - float(q[i, j])
+                sum += diff ** 2
     return sum
 
-def choose_q(target_region_mask, p, im):
+def choose_q(target_region_mask, p,p_mask,im):
     D={}
     #psi = gray_image_matrix - omega #déterminer comment déterminer psi à partir de omega
     #print(psi)
@@ -44,64 +51,108 @@ def choose_q(target_region_mask, p, im):
     #psi_image.show()
     #print("psi.shape:",psi.shape)
     source_region_mask = np.logical_not(target_region_mask)
+    print("source_region_mask.shape:",source_region_mask.shape)
     for i in range(source_region_mask.shape[0]-9):
         for j in range(source_region_mask.shape[1]-9):
             q_mask = source_region_mask[i:i+9,j:j+9]
-            if q_mask == np.array([[True for i in range(9)] for j in range(9)]):
+            #print("q_mask.shape:",q_mask.shape)
+            valid_patch = True
+            for k in range(9):
+                for l in range(9):
+                    if q_mask[k,l] == False:
+                        valid_patch = False #on peut mettre un break après pour minimiser le nombre d'itérations
+            if valid_patch:
                 q = im[i:i+9,j:j+9]
-                d = calcul_dist(p,q)
+                d = calcul_dist(p,q,p_mask)
                 D[(i,j)]=d
-            #print("q.shape:",q.shape)
-            #print("q:",q)
-            #print("d:",d)
     minimum_D = min(D, key=D.get) # renvoie la clé de la valeur minimale
     q_opt = im[minimum_D[0]:minimum_D[0]+9,minimum_D[1]:minimum_D[1]+9]
 
     return q_opt 
 
-
-#omega = np.zeros((61,61))
-#omega[12:48,12:48] = gray_image_matrix[12:48,12:48] # patch 10
+## Test
 
 target_region_mask = np.array([[False for i in range(61)] for j in range(61)])
 target_region_mask[12:48,12:48] = True 
-
-test = gray_image_matrix.copy()
-
-test[12:48,12:48] = None # mauvais type, à voir comment changer ça
-test_image = Image.fromarray(test)
-test_image.show()
+print("target_region_shape: ",target_region_mask.shape)
 
 
-
-p1 = test[8:17,8:17] # patch 9
-p1_image = Image.fromarray(p1)
-p1_image.show()
-p2 = test[44:53,44:53] # patch 9
-p2_image = Image.fromarray(p2)
-p2_image.show()
 
 new_matrix = gray_image_matrix.copy()
 
-D = choose_q(target_region_mask, p1, gray_image_matrix)
-#m = min(D, key=D.get) # renvoie la clé de la valeur minimale
-#mat = gray_image_matrix-omega
-#m_matrix = mat[m[0]:m[0]+9,m[1]:m[1]+9]
-#print(m)
+new_matrix_mask = new_matrix.copy()
+for i in range(61):
+    for j in range(61):
+        if target_region_mask[i,j] == True:
+            new_matrix_mask[i,j] = 0
 
-#print(m_matrix)
-#print(m_matrix.shape)
-print(p1)
-new_matrix[8:17,8:17] = D
+new_matrix_image = Image.fromarray(new_matrix_mask)
+new_matrix_image.show()
 
-D2 = choose_q(target_region_mask, p2, gray_image_matrix)
-m2 = min(D2, key=D2.get) # renvoie la clé de la valeur minimale
-#mat = gray_image_matrix-omega
-#m2_matrix = mat[m2[0]:m2[0]+9,m2[1]:m2[1]+9]
-#print(m2)
-#print(m2_matrix)
-#print(m2_matrix.shape)
-print(p2)
+## écrire la fonction copy image data
+
+
+p1 = new_matrix[8:17,8:17] # patch 9
+p1_mask = np.array([[True for i in range(9)] for j in range(9)])
+p1_mask [12:17,12:17] = False
+
+p1_image = Image.fromarray(p1)
+#p1_image.show()
+
+
+D1 = choose_q(target_region_mask, p1, p1_mask, new_matrix)
+
+D1_image = Image.fromarray(D1)
+D1_image.show()
+
+
+new_matrix[8:17,8:17] = D1
+target_region_mask[8:17,8:17] = False
+
+p2 = new_matrix[44:53,44:53] # patch 9
+p2_mask = np.array([[True for i in range(9)] for j in range(9)])
+p2_mask [44:48,44:48] = False # attention dépend de target region mask or à chaque itération, target region mask change, pour l'instant on ne prend pas ça en compte, mais il le faut
+
+p2_image = Image.fromarray(p2)
+#p2_image.show()
+
+
+D2 = choose_q(target_region_mask, p2,p2_mask, new_matrix)
+
+D2_image = Image.fromarray(D2)
+D2_image.show()
+
 new_matrix[44:53,44:53] = D2
+target_region_mask[44:53,44:53] = False
+
+
+p3 = new_matrix[8:17,44:53]
+p3_mask = np.array([[True for i in range(9)] for j in range(9)])
+p3_mask [12:17,44:48] = False
+
+D3 = choose_q(target_region_mask, p3,  p3_mask, new_matrix)
+
+D3_image = Image.fromarray(D3)
+D3_image.show()
+
+new_matrix[8:17,44:53] = D3
+
+target_region_mask[8:12,44:53] = False
+
+
+
+p4 = new_matrix[30:39,5:14]
+p4_mask = np.array([[True for i in range(9)] for j in range(9)])
+p4_mask [30:39,12:14] = False
+D4 = choose_q(target_region_mask, p4,  p4_mask, new_matrix)
+
+D4_image = Image.fromarray(D4)
+D4_image.show()
+
+new_matrix[30:39,5:14] = D4
+
+target_region_mask[8:12,44:53] = False
+
+
 new_matrix_image = Image.fromarray(new_matrix)
 new_matrix_image.show()
