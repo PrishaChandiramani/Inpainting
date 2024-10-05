dedb57db9afef297133011396d9f1aaef2669b38
Prisha-Chandiramani <154550366+Prisha-Chandiramani@users.noreply.github.com>
2024-10-05 22:35:11 +0200

    les tests donnent quelque chose, à optimiser

--- optimal_patch_search.py
+++ optimal_patch_search.py
@@ -43,7 +43,7 @@ def calcul_dist(p,q, p_mask):
                 sum += diff ** 2
     return sum
 
-def choose_q(target_region_mask, p,p_mask,im):
+def choose_q(target_region_mask, p,p_mask,im, patch_size):
     D={}
     #psi = gray_image_matrix - omega #déterminer comment déterminer psi à partir de omega
     #print(psi)
@@ -52,21 +52,21 @@ def choose_q(target_region_mask, p,p_mask,im):
     #print("psi.shape:",psi.shape)
     source_region_mask = np.logical_not(target_region_mask)
     print("source_region_mask.shape:",source_region_mask.shape)
-    for i in range(source_region_mask.shape[0]-9):
-        for j in range(source_region_mask.shape[1]-9):
-            q_mask = source_region_mask[i:i+9,j:j+9]
+    for i in range(source_region_mask.shape[0]-patch_size):
+        for j in range(source_region_mask.shape[1]-patch_size):
+            q_mask = source_region_mask[i:i+patch_size,j:j+patch_size]
             #print("q_mask.shape:",q_mask.shape)
             valid_patch = True
-            for k in range(9):
-                for l in range(9):
+            for k in range(patch_size):
+                for l in range(patch_size):
                     if q_mask[k,l] == False:
                         valid_patch = False #on peut mettre un break après pour minimiser le nombre d'itérations
             if valid_patch:
-                q = im[i:i+9,j:j+9]
+                q = im[i:i+patch_size,j:j+patch_size]
                 d = calcul_dist(p,q,p_mask)
                 D[(i,j)]=d
     minimum_D = min(D, key=D.get) # renvoie la clé de la valeur minimale
-    q_opt = im[minimum_D[0]:minimum_D[0]+9,minimum_D[1]:minimum_D[1]+9]
+    q_opt = im[minimum_D[0]:minimum_D[0]+patch_size,minimum_D[1]:minimum_D[1]+patch_size]
 
     return q_opt 
 
@@ -100,7 +100,7 @@ p1_image = Image.fromarray(p1)
 #p1_image.show()
 
 
-D1 = choose_q(target_region_mask, p1, p1_mask, new_matrix)
+D1 = choose_q(target_region_mask, p1, p1_mask, new_matrix,9)
 
 D1_image = Image.fromarray(D1)
 D1_image.show()
@@ -117,7 +117,7 @@ p2_image = Image.fromarray(p2)
 #p2_image.show()
 
 
-D2 = choose_q(target_region_mask, p2,p2_mask, new_matrix)
+D2 = choose_q(target_region_mask, p2,p2_mask, new_matrix,9)
 
 D2_image = Image.fromarray(D2)
 D2_image.show()
@@ -130,7 +130,7 @@ p3 = new_matrix[8:17,44:53]
 p3_mask = np.array([[True for i in range(9)] for j in range(9)])
 p3_mask [12:17,44:48] = False
 
-D3 = choose_q(target_region_mask, p3,  p3_mask, new_matrix)
+D3 = choose_q(target_region_mask, p3,  p3_mask, new_matrix,9)
 
 D3_image = Image.fromarray(D3)
 D3_image.show()
@@ -144,7 +144,7 @@ target_region_mask[8:12,44:53] = False
 p4 = new_matrix[30:39,5:14]
 p4_mask = np.array([[True for i in range(9)] for j in range(9)])
 p4_mask [30:39,12:14] = False
-D4 = choose_q(target_region_mask, p4,  p4_mask, new_matrix)
+D4 = choose_q(target_region_mask, p4,  p4_mask, new_matrix,9)
 
 D4_image = Image.fromarray(D4)
 D4_image.show()
@@ -156,3 +156,52 @@ target_region_mask[8:12,44:53] = False
 
 new_matrix_image = Image.fromarray(new_matrix)
 new_matrix_image.show()
+
+
+## nouvelles fonctions
+
+
+def update_target_region_mask(target_region_mask, selected_pixel, patch_size,im):
+    new_target_region_mask = np.copy(target_region_mask)
+    half_patch_size = patch_size // 2
+    for x in range(max(selected_pixel[0] - half_patch_size, 0), min(selected_pixel[0] + half_patch_size + 1, im.shape[0] - 1)):
+        for y in range(max(selected_pixel[1] - half_patch_size, 0), min(selected_pixel[1] + half_patch_size + 1, im.shape[1] - 1)):
+            if target_region_mask[x, y]:
+                new_target_region_mask[x, y] = False
+    return new_target_region_mask
+
+def patch_search(target_region_mask, im, patch_size):
+    # changer im en im_matrix pour être plus cohérent
+    new_matrix = im.copy()
+    half_patch_size = patch_size // 2
+    for i in range(target_region_mask.shape[0]):
+        for j in range(target_region_mask.shape[1]):
+            if target_region_mask[i,j] == True:
+                patch = im[max(i - half_patch_size, 0):min(i+ half_patch_size + 1, im.shape[0] - 1),max(j - half_patch_size, 0):min(j + half_patch_size + 1, im.shape[1] - 1)]
+                patch_mask = np.array([[True for i in range(patch_size)] for j in range(patch_size)])
+                # on met à False les valeurs de patch_mask qui correspondent à des valeurs de target_region_mask à True
+                #for k in range(patch_size): # modiifier ce qu'il y a à l'intérieur des range
+                    #for l in range(patch_size):
+                        #if target_region_mask[i,j] == True:
+                            #patch_mask[k,l] = False
+                q_patch = choose_q(target_region_mask, patch,  patch_mask, new_matrix, patch_size)
+                new_matrix [max(i - half_patch_size, 0):min(i+ half_patch_size + 1, im.shape[0] - 1),max(j - half_patch_size, 0):min(j + half_patch_size + 1, im.shape[1] - 1)] = q_patch
+                update_target_region_mask(target_region_mask, (i,j), patch_size,im)
+    return new_matrix
+
+## nouveaux tests
+target_region_mask2 = np.array([[False for i in range(61)] for j in range(61)])
+target_region_mask2[12:48,12:48] = True 
+
+image_initiale_matrix = gray_image_matrix.copy()
+image_initiale_matrix[12:48,12:48] = 0
+image_initiale = Image.fromarray(image_initiale_matrix)
+image_initiale.show()
+
+# si patch_size = 9, c'est plus long à éxecuter
+#test2 = patch_search(target_region_mask2, gray_image_matrix,9)
+#test2 = patch_search(target_region_mask2, gray_image_matrix,3)
+
+test2_image =Image.fromarray(test2)
+
+test2_image.show()
