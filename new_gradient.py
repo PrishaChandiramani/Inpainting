optimisation calcul gradient et fix des bugs sur les bords

new_gradient.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 from scipy import signal
 
-def region3x3(x, y, img, target_region_mask, depth):
+def region3x3(x, y, img, target_region_mask, maxdepth):
     size_x = img.shape[0]
     size_y = img.shape[1]
 
@@ -11,32 +11,37 @@ def region3x3(x, y, img, target_region_mask, depth):
     xmax = int(x + 1 % size_x)
     ymin = int(y - 1 % size_y)
     ymax = int(y + 1 % size_y)
-    local_target_region_mask = np.array([
-        [target_region_mask[xmin, ymin], target_region_mask[x, ymin], target_region_mask[xmax, ymin]],
-        [target_region_mask[xmin, y], target_region_mask[x, y], target_region_mask[xmax, y]],
-        [target_region_mask[xmin, ymax], target_region_mask[x, ymax], target_region_mask[xmax, ymax]]
-    ])
-    if np.any(local_target_region_mask) and depth > 0:
-        
-        x_matrix = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
-        y_matrix = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
 
-        new_x = int(x + np.sign(np.sum(x_matrix * local_target_region_mask)))
-        new_y = int(y + np.sign(np.sum(y_matrix * local_target_region_mask)))
-        return region3x3(new_x, new_y, img, target_region_mask, depth-1)
-    else:
-        result = np.array([
-            [img[xmin, ymin], img[xmin, y], img[xmin, ymax]], 
-            [img[x, ymin], img[x, y], img[x, ymax]], 
-            [img[xmax, ymin], img[xmax, y], img[xmax, ymax]]
-        ])
-        return result
+    x_matrix = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+    y_matrix = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
+
+    local_target_region_mask = target_region_mask[xmin:xmax + 1, ymin:ymax + 1]
+
+    depth = maxdepth
+    while depth > 0 and np.any(local_target_region_mask):
+        
+        x = int(x + np.sign(np.sum(x_matrix * local_target_region_mask)))
+        y = int(y + np.sign(np.sum(y_matrix * local_target_region_mask)))
+
+        if x == 1 or x == size_x - 2 or y == 1 or y == size_y - 2:
+            break
+        
+        xmin = int(x - 1 % size_x)
+        xmax = int(x + 1 % size_x)
+        ymin = int(y - 1 % size_y)
+        ymax = int(y + 1 % size_y)
+
+        local_target_region_mask = target_region_mask[xmin:xmax + 1, ymin:ymax + 1]
+        depth -= 1
+
+    result = img[xmin:xmax + 1, ymin:ymax + 1]
+    return result
     
 def new_gradient(pixel, image, target_region_mask):
     gradient = [0. , 0.]
     x, y = pixel[0], pixel[1]
 
-    pixel_region = region3x3(x, y, image, target_region_mask, 5)
+    pixel_region = region3x3(x, y, image, target_region_mask, 3)
     
     gradient_core_x = 1/4 * np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
     gradient_core_y = 1/4 * np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])